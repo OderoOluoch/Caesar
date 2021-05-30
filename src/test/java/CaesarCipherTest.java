d41e9c2a7bdd2fbc71f638cedbc805c832374398
OderoOluoch <oderoluoch@gmail.com>
2021-05-30 12:39:41 +0300

    Tested correct instantiation of the CaesarCipher class with the required parameters

--- src/test/java/CaesarCipherTest.java
+++ src/test/java/CaesarCipherTest.java
@@ -1,5 +1,11 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CaesarCipherTest {
-
+    @Test
+    public void newCaesarCipher_instantiatesCorrectly(){
+        CaesarCipher testCaesarCipher = new CaesarCipher("Happy Coding",5);
+        assertEquals(true,testCaesarCipher instanceof CaesarCipher);
+    }
 }
