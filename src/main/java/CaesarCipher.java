04c53306ac037edff9db4d870c849566d551eee9
OderoOluoch <oderoluoch@gmail.com>
2021-05-30 12:27:14 +0300

    Created a constructor for the CaesarCipher class

--- src/main/java/CaesarCipher.java
+++ src/main/java/CaesarCipher.java
@@ -1,2 +1,9 @@
 public class CaesarCipher {
+    private String mMessage;
+    private int shiftKey;
+
+    public CaesarCipher(String message, int key){
+        this.mMessage = message;
+        this.shiftKey=key;
+    }
 }
