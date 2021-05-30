1eb4186dc644c6f7da6938f3a031dacf828a7d8b
OderoOluoch <oderoluoch@gmail.com>
2021-05-30 19:15:39 +0300

    Failing test on the message Encryption method

--- src/test/java/CaesarCipherTest.java
+++ src/test/java/CaesarCipherTest.java
@@ -5,25 +5,31 @@ import static org.junit.jupiter.api.Assertions.*;
 class CaesarCipherTest {
     @Test
     public void newCaesarCipher_instantiatesCorrectly(){
-        CaesarCipher testCaesarCipher = new CaesarCipher("message",5);
+        CaesarCipher testCaesarCipher = new CaesarCipher("hi",5);
         assertEquals(true,testCaesarCipher instanceof CaesarCipher);
     }
 
     @Test
-    public void newCaesarCipher_getMessage_message() {
-        CaesarCipher testCaesarCipher = new CaesarCipher("message",5);
+    public void newCaesarCipher_getMessage_hi() {
+        CaesarCipher testCaesarCipher = new CaesarCipher("hi",5);
         assertEquals("message",testCaesarCipher.getMessage());
     }
 
     @Test
     void newCaesarCipher_getShiftKey_5(){
-        CaesarCipher testCaesarCipher = new CaesarCipher("message",5);
+        CaesarCipher testCaesarCipher = new CaesarCipher("hi",5);
         assertEquals(5,testCaesarCipher.getShiftKey());
     }
 
     @Test
     void newCaesarCipher_manipulateShiftKeyToGetRightLetter_5(){
-        CaesarCipher testCaesarCipher = new CaesarCipher("message",85);
+        CaesarCipher testCaesarCipher = new CaesarCipher("hi",5);
         assertEquals(5,testCaesarCipher.shiftKeyForEncryption());
+    }
+
+    @Test
+    void newCaesarCipher_manipulateMessageToEnforceEncryption_mn(){
+        CaesarCipher testCaesarCipher = new CaesarCipher("hii",5);
+        assertEquals("hi",testCaesarCipher.messageEncryption());
     }
 }
