334afbc2de8827f8dfe113a968c903e89d3d39b4
OderoOluoch <oderoluoch@gmail.com>
2021-05-30 22:29:36 +0300

    Impemented the Scanner object to take in user input

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println();
+        Scanner myScanner = new Scanner(System.in);
+
+        System.out.println("Want to send a secret message? \n Enter the message here to Encrypt your message: ");
+        String message = myScanner.nextLine();
+        System.out.println(message);
     }
 }
