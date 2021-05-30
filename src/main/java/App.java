590f51fc85f8a4074055ed58592ff4cdd1cef9be
OderoOluoch <oderoluoch@gmail.com>
2021-05-30 22:39:18 +0300

    Impemented User is able to enter message and a secret key

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -6,6 +6,23 @@ public class App {
 
         System.out.println("Want to send a secret message? \n Enter the message here to Encrypt your message: ");
         String message = myScanner.nextLine();
+        System.out.println("Now enter a number, to serve as your encryption key");
+        int key = myScanner.nextInt();
+         CaesarCipher caesarCipher = new CaesarCipher(message,key);
+         String encryptedMessage = caesarCipher.messageEncryption();
+         String decryptedMessage = caesarCipher.messageDecryption();
+
+        System.out.println("********************************************");
+        System.out.println("Your Original Message: ");
         System.out.println(message);
+        System.out.println("********************************************");
+        System.out.println("Your Encrypted Message: ");
+        System.out.println(encryptedMessage);
+        System.out.println("********************************************");
+        System.out.println("Your Decrypted Message: ");
+        System.out.println(decryptedMessage);
+
+
+
     }
 }
