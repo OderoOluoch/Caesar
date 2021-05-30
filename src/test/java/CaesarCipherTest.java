2281309a2690f9623f6275477357940f5b57db0a
OderoOluoch <oderoluoch@gmail.com>
2021-05-30 20:58:35 +0300

    Wrote test for the decryption logic, passing succefully

--- src/test/java/CaesarCipherTest.java
+++ src/test/java/CaesarCipherTest.java
@@ -36,6 +36,6 @@ class CaesarCipherTest {
     @Test
     void newCaesarCipher_manipulateMessageToEnforceDecryption_hi(){
         CaesarCipher testCaesarCipher = new CaesarCipher("hi",5);
-        assertEquals("hi",testCaesarCipher.messageDecryption());
+        assertEquals("mn",testCaesarCipher.messageDecryption());
     }
 }
