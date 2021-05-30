e02d4bbd9ba7f1bb8229d0207abc0bf510ea860f
OderoOluoch <oderoluoch@gmail.com>
2021-05-30 17:49:21 +0300

    structured behaviour to be tested before it is implemented

--- src/main/java/CaesarCipher.java
+++ src/main/java/CaesarCipher.java
@@ -12,4 +12,70 @@ public class CaesarCipher {
     public int getShiftKey(){
         return this.shiftKey;
     }
+
+    public String encrypt(){
+        if(this.shiftKey > 76){
+            this.shiftKey = this.shiftKey % 26;
+        }else if(this.shiftKey < 0){
+            this.shiftKey = (this.shiftKey % 26)+26;
+        }
+        String cipherText = "";
+        int length = mMessage.length();
+        for(int i =0; i<length;i++){
+            char ch = this.mMessage.charAt(i);
+            if(Character.isLetter(ch)){
+                if(Character.isLowerCase(ch)){
+                    char c = (char)(ch + this.shiftKey);
+                    if(c>'z'){
+                        cipherText += (char)(ch -(26 - this.shiftKey));
+                    }else{
+                        cipherText += c;
+                    }
+                }else if(Character.isUpperCase(ch)){
+                    char c = (char)(ch+ this.shiftKey);
+                    if(c>'Z'){
+                        cipherText += (char)(ch -(26 - this.shiftKey));
+                    }else{
+                        cipherText += c;
+                    }
+                }
+            }else {
+                cipherText += ch;
+            }
+        }
+        return cipherText;
+    }
+
+    public String decrypt(){
+        if(this.shiftKey >76){
+            this.shiftKey = this.shiftKey % 26;
+        }else if(this.shiftKey <0){
+            this.shiftKey = (this.shiftKey % 26)+26;
+        }
+        String cipherText = "";
+        int length = this.mMessage.length();
+        for(int i =0; i<length;i++){
+            char ch = this.mMessage.charAt(i);
+            if(Character.isLetter(ch)){
+                if(Character.isLowerCase(ch)){
+                    char c = (char)(ch - this.shiftKey);
+                    if(c<'a'){
+                        cipherText += (char)(ch +(26- this.shiftKey));
+                    }else{
+                        cipherText += c;
+                    }
+                }else if(Character.isUpperCase(ch)){
+                    char c = (char)(ch- this.shiftKey);
+                    if(c<'A'){
+                        cipherText += (char)(ch + (26 - this.shiftKey));
+                    }else{
+                        cipherText += c;
+                    }
+                }
+            }else {
+                cipherText += ch;
+            }
+        }
+        return cipherText;
+    }
 }
